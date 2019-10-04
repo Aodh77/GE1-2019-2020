d5b3ec677cad2bf2990e38d0646250ac469cac09
Aodh o leary <aodh77@gmail.com>
2019-10-04 15:22:17 +0100

    hi

---
+++ GE1Examples2019/Assets/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    public float speed = 10;
+    // Start is called before the first frame update
+    void Start()
+    {
+       // Destroy(GameObject, 5);
+    }
+
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
